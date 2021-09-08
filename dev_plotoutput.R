dabfe9f6a5116b1b66766fdfd3e818685f7a5261
Sorrieah <sorrieah@gmail.com>
2021-09-08 19:09:10 +1000

    Add dev_plotoutput.R

---
+++ dev_plotoutput.R
@@ -0,0 +1,23 @@
+source("ZooMizerPlots.R")
+source("ZooMizerSummaryFunctions.R")
+
+# Default phyto_fixed resource spectrum on grid cell 703
+jobname <- "20210823_grid_sst-16_chlo100-398"
+
+# Load RDS files and remove fish from ZooMSS output
+zoomss <- readRDS(paste0("Output/", jobname, "_ZooMSS_", ID_char,".RDS"))
+zoo_params <- removeSpecies(zoomss@params, c("Fish_Small","Fish_Med","Fish_Large"))
+out <- readRDS(paste0("Output/", jobname, "_ZooMizer_", ID_char,".RDS"))
+
+
+
+plot(out)
+plot(zoomss)
+
+# Plot the abundance spectra including ZooMizer resource
+plotSpectra_ZooMizer(out, zoo_params) #check to see if there's a way to only plot Resource
+# Plot the abundance of species through time
+plotAbundance_ZooMizer(out, zoo_params)
+# Plot the biomass of species through time
+plotBiomass_ZooMizer(out, zoo_params)
+
