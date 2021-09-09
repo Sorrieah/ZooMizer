0d1fe58120a18019cb2a2f4fd7b55c9c25dd43a8
Sorrieah <sorrieah@gmail.com>
2021-09-09 16:15:26 +1000

    Add plots for decrement run, show resource spectrum in size domain

--- dev_plotoutput.R
+++ dev_plotoutput.R
@@ -3,6 +3,8 @@ source("ZooMizerSummaryFunctions.R")
 
 # Default phyto_fixed resource spectrum on grid cell 703
 jobname <- "20210823_grid_sst-16_chlo100-398"
+# Using phyto_decrement for 1% resource spectrum decrease each time step
+jobname <- "202109091130_grid_sst-16_chlo100-398"
 
 # Load RDS files and remove fish from ZooMSS output
 zoomss <- readRDS(paste0("Output/", jobname, "_ZooMSS_", ID_char,".RDS"))
@@ -13,9 +15,11 @@ out <- readRDS(paste0("Output/", jobname, "_ZooMizer_", ID_char,".RDS"))
 
 plot(out)
 plot(zoomss)
+plot(zoo_params)
 
 # Plot the abundance spectra including ZooMizer resource
 plotSpectra_ZooMizer(out, zoo_params) #check to see if there's a way to only plot Resource
+plotSpectra_ZooMizer(out, zoo_params, wlim = c(1e-15, NA))
 # Plot the abundance of species through time
 plotAbundance_ZooMizer(out, zoo_params)
 # Plot the biomass of species through time
