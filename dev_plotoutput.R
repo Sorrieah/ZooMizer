a3d2d2b0fb6429eca08fc32226248cf4d0ec12d0
Sorrieah <sorrieah@gmail.com>
2021-09-10 15:15:09 +1000

    draft plot function

--- dev_plotoutput.R
+++ dev_plotoutput.R
@@ -11,11 +11,8 @@ zoomss <- readRDS(paste0("Output/", jobname, "_ZooMSS_", ID_char,".RDS"))
 zoo_params <- removeSpecies(zoomss@params, c("Fish_Small","Fish_Med","Fish_Large"))
 out <- readRDS(paste0("Output/", jobname, "_ZooMizer_", ID_char,".RDS"))
 
-
-
 plot(out)
 plot(zoomss)
-plot(zoo_params)
 
 # Plot the abundance spectra including ZooMizer resource
 plotSpectra_ZooMizer(out, zoo_params) #check to see if there's a way to only plot Resource
@@ -25,3 +22,21 @@ plotAbundance_ZooMizer(out, zoo_params)
 # Plot the biomass of species through time
 plotBiomass_ZooMizer(out, zoo_params)
 
+# Attempt at making an auto-plot function
+# dev_plot <- function(fish_object, 
+#                      zoo_object, 
+#                      jobname = jobname, 
+#                      resource_function = resource_dynamics){
+#   save_path <- paste0("Output/",jobname,"/")
+#   zoo_params <- removeSpecies(zoo_object@params, c("Fish_Small","Fish_Med","Fish_Large"))
+#   plot(fish_object)
+#   ggsave(paste0(save_path,jobname,"_fish.png"), width = 862, height = 550, units = "px")
+#   plot(zoo_object)
+#   ggsave(paste0(save_path,jobname,"_zoo.png"), width = 862, height = 550, units = "px")
+#   plotSpectra_ZooMizer(fish_object, zoo_params, wlim = c(1e-15, NA))
+#   ggsave(paste0(save_path,jobname,"_spectra.png"), width = 862, height = 550, units = "px")
+#   plotAbundance_ZooMizer(fish_object, zoo_params)
+#   ggsave(paste0(save_path,jobname,"_abundance.png"), width = 862, height = 550, units = "px")
+#   plotBiomass_ZooMizer(fish_object, zoo_params)
+#   ggsave(paste0(save_path,jobname,"_biomass.png"), width = 862, height = 550, units = "px")
+# }
